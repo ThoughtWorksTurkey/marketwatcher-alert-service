0d88500065de24e4824bc043b47a19f099532084
Ayse Yorusun <ayorusun@euayorusan.local>
2016-05-30 16:30:35 +0300

    Red / Green / Refactor

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-// Alert is
+import (
+	"errors"
+)
+
+// Alert is the primary entity of this microservice
 type Alert struct {
 	id                 int
 	name               string
@@ -8,5 +12,32 @@ type Alert struct {
 	niceToHaveCriteria []string
 	excludedCriteria   []string
 	threshold          int
-	ownerid            int64
+	ownerID            int64
+}
+
+func (a *Alert) validate() bool {
+	return a.id > 0 &&
+		a.name != "" &&
+		a.requiredCriteria != nil && len(a.requiredCriteria) > 0 &&
+		a.threshold > 0 && a.ownerID > 0
+}
+
+var alertMap = make(map[int]Alert)
+
+func findAlertByID(id int) Alert {
+	v, _ := alertMap[id]
+	return v
+}
+
+func insertAlert(a Alert) (Alert, error) {
+	if !a.validate() {
+		return Alert{}, errors.New("Validation failed")
+	}
+	alertMap[a.id] = a
+	v, ok := alertMap[a.id]
+	if ok {
+		return v, nil
+	}
+
+	return v, errors.New("Could NOT insert Alert")
 }
