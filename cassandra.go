6ebcd7562fea324f55c883b350ede40757222f99
Zehra Kavasoglu <zehra@euzehra.local>
2016-06-23 17:43:47 +0300

    #13 change cassandra method to success tests

--- cassandra.go
+++ cassandra.go
@@ -14,8 +14,56 @@ var cassandraKeySpaceName = "marketwatcher"
 var session *gocql.Session
 var cassandraConnectTimeout = 10 * time.Second
 var initialCqlFile = "/data/init.cql"
+var connectionEstablished = false
 
-func init() {
+var find = func(id gocql.UUID) (Alert, error) {
+	if connectionEstablished == false {
+		connectToCassandra()
+		connectionEstablished = true
+	}
+
+	result := Alert{}
+
+	if err := session.Query(`SELECT id, owner_id, name, required_criteria, nice_to_have_criteria, excluded_criteria, threshold, status FROM alert WHERE id = ?`,
+		id).Consistency(gocql.One).Scan(&result.ID, &result.OwnerID, &result.Name, &result.RequiredCriteria, &result.NiceToHaveCriteria, &result.ExcludedCriteria, &result.Threshold, &result.Status); err != nil {
+		log.Println("Find returns no records, check given parameters: ", err)
+	}
+
+	return result, nil
+}
+
+var findByOwner = func(ownerID int) ([]Alert, error) {
+	if connectionEstablished == false {
+		connectToCassandra()
+		connectionEstablished = true
+	}
+
+	result := Alert{}
+	alerts := []Alert{}
+	iter := session.Query(`SELECT id, owner_id, name, required_criteria, nice_to_have_criteria, excluded_criteria, threshold, status FROM alert WHERE owner_id = ?`, strconv.Itoa(ownerID)).Iter()
+	for iter.Scan(&result.ID, &result.OwnerID, &result.Name, &result.RequiredCriteria, &result.NiceToHaveCriteria, &result.ExcludedCriteria, &result.Threshold, &result.Status) {
+		alerts = append(alerts, result)
+	}
+	err := iter.Close()
+	return alerts, err
+}
+
+var save = func(a Alert) (Alert, error) {
+	if connectionEstablished == false {
+		connectToCassandra()
+		connectionEstablished = true
+	}
+	a.ID = GenerateAlertId()
+
+	if err := session.Query(`INSERT INTO alert (id, owner_id, name, required_criteria, nice_to_have_criteria, excluded_criteria, threshold, status)
+	VALUES (?,?,?,?,?,?,?,?)`, a.ID, a.OwnerID, a.Name, a.RequiredCriteria, a.NiceToHaveCriteria, a.ExcludedCriteria, a.Threshold, a.Status).Exec(); err != nil {
+		log.Fatal(err)
+	}
+
+	return find(a.ID)
+}
+
+func connectToCassandra() {
 	cluster := gocql.NewCluster(strings.Split(os.Getenv("CASSANDRA_NODES"), ",")...)
 	cluster.Timeout = cassandraConnectTimeout
 
@@ -52,37 +100,4 @@ func executeInitialQuery(cl *gocql.ClusterConfig) {
 			log.Fatal("Could NOT execute initial query: ", err)
 		}
 	}
-}
-
-var find = func(id gocql.UUID) (Alert, error) {
-	result := Alert{}
-
-	if err := session.Query(`SELECT id, owner_id, name, required_criteria, nice_to_have_criteria, excluded_criteria, threshold, status FROM alert WHERE id = ?`,
-		id).Consistency(gocql.One).Scan(&result.ID, &result.OwnerID, &result.Name, &result.RequiredCriteria, &result.NiceToHaveCriteria, &result.ExcludedCriteria, &result.Threshold, &result.Status); err != nil {
-		log.Println("Find returns no records, check given parameters: ", err)
-	}
-
-	return result, nil
-}
-
-var findByOwner = func(ownerID int) ([]Alert, error) {
-	result := Alert{}
-	alerts := []Alert{}
-	iter := session.Query(`SELECT id, owner_id, name, required_criteria, nice_to_have_criteria, excluded_criteria, threshold, status FROM alert WHERE owner_id = ?`, strconv.Itoa(ownerID)).Iter()
-	for iter.Scan(&result.ID, &result.OwnerID, &result.Name, &result.RequiredCriteria, &result.NiceToHaveCriteria, &result.ExcludedCriteria, &result.Threshold, &result.Status) {
-		alerts = append(alerts, result)
-	}
-	err := iter.Close()
-	return alerts, err
-}
-
-var save = func(a Alert) (Alert, error) {
-	a.ID = GenerateAlertId()
-
-	if err := session.Query(`INSERT INTO alert (id, owner_id, name, required_criteria, nice_to_have_criteria, excluded_criteria, threshold, status)
-	VALUES (?,?,?,?,?,?,?,?)`, a.ID, a.OwnerID, a.Name, a.RequiredCriteria, a.NiceToHaveCriteria, a.ExcludedCriteria, a.Threshold, a.Status).Exec(); err != nil {
-		log.Fatal(err)
-	}
-
-	return find(a.ID)
 }
