5061e8843c1bd3733b51ea911962a9c928178869
Zehra Kavasoglu <zehra@euzehra.local>
2016-06-23 12:25:55 +0300

    #13 @cengizIO change alert service paths

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,8 +11,21 @@ ecs-cli configure \
 	--compose-project-name-prefix "" \
 	--compose-service-name-prefix ""
 
+CONFIGURE_RESULT=$?
+if [ $CONFIGURE_RESULT -ne 0 ]; then
+	echo "Could not configure ECS CLI"
+	exit $CONFIGURE_RESULT
+fi
+
 # ecs-cli up --keypair marketwatcher --capability-iam --size 1 --instance-type t2.medium
 
+COMPOSE_PROJECT_NAME=marketwatcher-alert-service \
 ecs-cli compose --file docker-compose.yml service up
+
+UP_RESULT=$?
+if [ $UP_RESULT -ne 0 ]; then
+	echo "Could not bring service UP in ECS"
+	exit $UP_RESULT
+fi
 
 cd $OLDPWD
