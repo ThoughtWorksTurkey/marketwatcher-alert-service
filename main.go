fbf22ebb987f02d16062a12f243c7f58e32416cd
Cengiz Can <cengizIO@users.noreply.github.com>
2016-07-11 19:51:08 +0300

    #13 remove allow origin filter

--- main.go
+++ main.go
@@ -4,20 +4,12 @@ package main
 
 import (
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/plugins/cors"
 )
 
 func main() {
 	beego.Router("/api/alerts/owner_id/:owner_id([0-9]+)", &AlertController{}, "get:GetAlertsOfOwner")
 	beego.Router("/api/alerts/id/:id", &AlertController{}, "get:GetAlertById")
 	beego.Router("/api/alerts", &AlertController{}, "post:PostNewAlert")
-
-	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-		AllowOrigins:     []string{"http://*"},
-		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
-		AllowCredentials: true,
-	}))
 
 	beego.Run()
 }
